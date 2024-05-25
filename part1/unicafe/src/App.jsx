84aacbe93060bbfa74d95094e2885558846462ca
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-25 14:31:04 -0300

    Complete app (ex1.6)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,7 +6,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>App unicafe</div>;
+  const handleClickGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <main>
+      <h1>Give feedback</h1>
+      <button onClick={handleClickGood}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </main>
+  );
 };
 
 export default App;
