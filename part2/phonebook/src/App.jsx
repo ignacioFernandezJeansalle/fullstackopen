9c1bf079e7254ce6c91ae5184743b778e2a88b99
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-27 14:58:43 -0300

    Add input and validation for numbers (ex2.8)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,38 +1,55 @@
 import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const handleChangeInput = (event) => {
+  const handleChangeNewName = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleChangeNewNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const handleSubmitForm = (event) => {
     event.preventDefault();
 
-    if (newName === "") return alert("Please enter a valid name");
+    if (!newName) return alert("Please enter a valid name");
+    if (!newNumber) return alert("Please enter a valid number");
 
     if (persons.some((person) => person.name === newName)) return alert(`${newName} is already added to phonebook`);
 
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
       <h1>Phonebook</h1>
       <form>
-        <label htmlFor="newName">name: </label>
-        <input id="newName" onChange={handleChangeInput} value={newName} />
-        <button type="submit" onClick={handleSubmitForm}>
-          add
-        </button>
+        <div>
+          <label htmlFor="newName">name: </label>
+          <input id="newName" onChange={handleChangeNewName} value={newName} />
+        </div>
+        <div>
+          <label htmlFor="newNumber">number: </label>
+          <input id="newNumber" onChange={handleChangeNewNumber} value={newNumber} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmitForm}>
+            add
+          </button>
+        </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(({ name }, index) => (
-          <li key={index}>{name}</li>
+        {persons.map(({ name, number }, index) => (
+          <li key={index}>
+            {name} {number}
+          </li>
         ))}
       </ul>
     </div>
