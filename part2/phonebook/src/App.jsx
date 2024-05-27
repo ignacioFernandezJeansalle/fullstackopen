fa4ffbc736c0ab58ee6f9d7a7bfbb787341bdebb
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-27 15:31:00 -0300

    Add filter to show persons (ex2.9)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [personsToShow, setPersonsToShow] = useState(persons);
+
+  const handleChangeNewFilter = (event) => {
+    const newFilter = event.target.value;
+    setFilter(newFilter);
+
+    const newPersonsToShow = !newFilter
+      ? persons
+      : persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()));
+
+    setPersonsToShow(newPersonsToShow);
+  };
 
   const handleChangeNewName = (event) => {
     setNewName(event.target.value);
@@ -21,14 +40,23 @@ function App() {
 
     if (persons.some((person) => person.name === newName)) return alert(`${newName} is already added to phonebook`);
 
-    setPersons(persons.concat({ name: newName, number: newNumber }));
+    const newPersons = persons.concat({ name: newName, number: newNumber });
+    setPersons(newPersons);
     setNewName("");
     setNewNumber("");
+    setFilter("");
+    setPersonsToShow(newPersons);
   };
 
   return (
     <div>
       <h1>Phonebook</h1>
+      <div>
+        <label htmlFor="newFilter">filter shown with </label>
+        <input id="newFilter" onChange={handleChangeNewFilter} value={filter} />
+      </div>
+
+      <h2>Add a new</h2>
       <form>
         <div>
           <label htmlFor="newName">name: </label>
@@ -44,9 +72,10 @@ function App() {
           </button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
-        {persons.map(({ name, number }, index) => (
+        {personsToShow.map(({ name, number }, index) => (
           <li key={index}>
             {name} {number}
           </li>
