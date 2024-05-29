f636e957531d38eebcd1ca59dea49492ce039d99
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-05-29 15:40:16 -0300

    Create component CountryData

---
+++ part2/countriesdata/src/components/CountryData.jsx
@@ -0,0 +1,21 @@
+export default function CountryData({ countryData }) {
+  if (!countryData) return null;
+
+  const { name, capital, area, languages, flags } = countryData;
+  const languagesArray = Object.values(languages);
+
+  return (
+    <div className="country-data">
+      <h2>{name.common}</h2>
+      <p>Capital: {capital}</p>
+      <p>Area: {area}</p>
+      <p>Languages:</p>
+      <ul>
+        {languagesArray.map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={flags.png} alt={flags.alt} />
+    </div>
+  );
+}
